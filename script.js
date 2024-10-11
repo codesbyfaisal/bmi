e547be2d50780bc75c3f114a6a5e05754eb0c8dd
codebyfaisal <fm0320939695@gmail.com>
2024-10-11 21:04:38 +0500

    add EventListener to update showing user h/w

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const weight = document.getElementById('weight')
 const showHeight = document.querySelector('.show-height')
 const showWeight = document.querySelector('.show-weight')
 const result = document.getElementById('result')
+
+height.addEventListener('input', () => {
+  showHeight.innerHTML = height.value
+})
+
+weight.addEventListener('input', () => {
+  showWeight.innerHTML = weight.value
+})
