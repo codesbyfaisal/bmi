45ff4763d1e9db743a2e7f379534aa5dd107cecc
codebyfaisal <fm0320939695@gmail.com>
2024-10-11 21:03:34 +0500

    js - declare constants

---
+++ script.js
@@ -0,0 +1,6 @@
+const app = document.querySelector('.app')
+const height = document.getElementById('height')
+const weight = document.getElementById('weight')
+const showHeight = document.querySelector('.show-height')
+const showWeight = document.querySelector('.show-weight')
+const result = document.getElementById('result')
